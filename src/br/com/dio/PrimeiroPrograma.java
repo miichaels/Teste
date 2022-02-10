c53a7b945758bcd18afca72a86f68cc7a56c646d
miichaels <michael_araujo2012@hotmail.com>
2022-02-10 10:24:10 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package br.com.dio;
+
+
+import model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+
+
+     /*  int a = 5;
+       int b = 5;
+        System.out.println("Hello word "+ (a+b));*/
+        System.out.println();
+        System.out.println();
+
+    }
+}
